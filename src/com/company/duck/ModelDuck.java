7dbfc9a21867549443791fead41747c958864bb6
osvical <osvical@gmail.com>
2018-06-20 01:15:35 +0300

    + class ModelDuck

--- src/com/company/duck/ModelDuck.java
+++ src/com/company/duck/ModelDuck.java
@@ -1,5 +1,15 @@
 package com.company.duck;
 
-public class ModelDuck {
+import com.company.behavior.FlyNoWay;
+import com.company.behavior.Quack;
 
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    public void display() {
+        System.out.println("I'm a model duck");
+    }
 }
